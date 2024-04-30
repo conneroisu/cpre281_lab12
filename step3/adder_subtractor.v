241a261526974364cdde1c68d4f133826c041f72
connero <88785126+conneroisu@users.noreply.github.com>
2024-04-30 14:36:25 -0500

    latest

--- step3/adder_subtractor.v
+++ step3/adder_subtractor.v
@@ -1,82 +1,57 @@
-module adder_subtractor_4bit (
-    X3,
-    Y3,
-    X2,
-    Y2,
-    X1,
-    Y1,
-    X0,
-    Y0,
-    Cin,
-    SubEn,
-    Cout,
-    S3,
-    S2,
-    S1,
-    S0,
-    Ov
+module adder_subtractor(
+	X3,
+	Y3,
+	X2,
+	Y2,
+	X1,
+	Y1,
+	X0,
+	Y0,
+	Control,
+	Overflow,
+	Cout,
+	S2,
+	S1,
+	S0,
+	S3
 );
-
-input wire X3;
-input wire Y3;
-input wire X2;
-input wire Y2;
-input wire X1;
-input wire Y1;
-input wire X0;
-input wire Y0;
-input wire Cin;
-input wire SubEn;
-output wire Cout;
-output wire S3;
-output wire S2;
-output wire S1;
-output wire S0;
-output wire Ov;
-
-wire SYNTHESIZED_WIRE_5;
-wire SYNTHESIZED_WIRE_1;
-wire SYNTHESIZED_WIRE_2;
-wire SYNTHESIZED_WIRE_3;
-wire [3:0] Y_inv;
-wire Cin_modified;
-
-assign S3 = SYNTHESIZED_WIRE_3;
-assign Y_inv = {Y3, Y2, Y1, Y0} ^ {4{SubEn}}; // Invert Y inputs when SubEn is 1
-assign Cin_modified = Cin | SubEn; // Set Cin to 1 when SubEn is 1
-
-FA b2v_inst (
-    .Cin(SYNTHESIZED_WIRE_5),
-    .X(X3),
-    .Y(Y_inv[3]),
-    .S(Cout),
-    .Cout(SYNTHESIZED_WIRE_3)
-);
-
-FA b2v_inst1 (
-    .Cin(SYNTHESIZED_WIRE_1),
-    .X(X2),
-    .Y(Y_inv[2]),
-    .S(S2),
-    .Cout(SYNTHESIZED_WIRE_5)
-);
-
-FA b2v_inst2 (
-    .Cin(SYNTHESIZED_WIRE_2),
-    .X(X1),
-    .Y(Y_inv[1]),
-    .S(S1),
-    .Cout(SYNTHESIZED_WIRE_1)
-);
-
-FA b2v_inst3 (
-    .Cin(Cin_modified),
-    .X(X0),
-    .Y(Y_inv[0]),
-    .S(S0),
-    .Cout(SYNTHESIZED_WIRE_2)
-);
-
-assign Ov = SYNTHESIZED_WIRE_3 ^ SYNTHESIZED_WIRE_5;
-
+input wire	X3;
+input wire	Y3;
+input wire	X2;
+input wire	Y2;
+input wire	X1;
+input wire	Y1;
+input wire	X0;
+input wire	Y0;
+input wire	Control;
+output wire	Overflow;
+output wire	Cout;
+output wire	S2;
+output wire	S1;
+output wire	S0;
+output wire	S3;
+wire	SYNTHESIZED_WIRE_0;
+wire	SYNTHESIZED_WIRE_1;
+wire	SYNTHESIZED_WIRE_2;
+wire	SYNTHESIZED_WIRE_3;
+assign	SYNTHESIZED_WIRE_0 = Control ^ Y3;
+assign	SYNTHESIZED_WIRE_1 = Control ^ Y2;
+assign	SYNTHESIZED_WIRE_2 = Control ^ Y1;
+assign	SYNTHESIZED_WIRE_3 = Control ^ Y0;
+adder_4bit	b2v_inst4(
+	.X3(X3),
+	.Y3(SYNTHESIZED_WIRE_0),
+	.X2(X2),
+	.Y2(SYNTHESIZED_WIRE_1),
+	.X1(X1),
+	.Y1(SYNTHESIZED_WIRE_2),
+	.Cin(Control),
+	.X0(X0),
+	.Y0(SYNTHESIZED_WIRE_3),
+	.Cout(Cout),
+	.S3(S3),
+	.Ov(Overflow),
+	.S2(S2),
+	.S1(S1),
+	.S0(S0));
 endmodule
